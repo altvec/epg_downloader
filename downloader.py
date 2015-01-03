c91daf811f913b4e2153907d172606ec1d64f219
altvec <sergey@bluehat.me>
2015-01-03 23:29:12 +0500

    Some cleanup and minor changes

--- downloader.py
+++ downloader.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- conding: utf8 -*-
 
 import re
 import os
@@ -8,31 +9,34 @@ import xml.etree.ElementTree as ET
 import subprocess
 
 config = ConfigParser.RawConfigParser()
-config.readfp(open(r'../settings.conf'))
+config.readfp(open(r'settings.conf'))
 username = config.get('Main', 'username')
 password = config.get('Main', 'password')
 
-out_path = "./files/"
-if not os.path.exists(os.path.dirname(out_path)):
-    os.makedirs(os.path.dirname(out_path))
+out_dir = "files/"
+if not os.path.exists(os.path.dirname(out_dir)):
+    os.makedirs(os.path.dirname(out_dir))
 
-base_url = 'http://xmltv.s-tv.ru/pers/%s/index.php?pass=%s' % (username,
-        password)
+url = 'http://xmltv.s-tv.ru/pers/%s/index.php?pass=%s' % (username, password)
 
-file = "base_source.xml"
-response = urllib2.urlopen(base_url)
+file = "stv.xml"
+response = urllib2.urlopen(url)
 with open(file, 'wb') as f:
     f.write(response.read())
 
 tree = ET.parse(file)
 root = tree.getroot()
 procs = []
+
+print "Downloading EPG files for %s channels. Please wait a bit" % (len(root))
+
 for child in root:
     channel_src = re.sub('&sh=.*', '', child.attrib['src'])
     channel_id = child.attrib['channel_id']
     procs.append(subprocess.Popen(['/usr/bin/curl', '-s', '-o',
-        out_path+"/"+channel_id+".xml", channel_src]))
+                                  out_dir+"/"+channel_id+".xml", channel_src]))
 for proc in procs:
     proc.wait()
 
 os.remove(file)
+print "All done. Have a nice day!"
